40b5cdd412f5908278c5994a6463c88cd031d906
MrSelfisH01 <108799020+MrSelfisH01@users.noreply.github.com>
2022-10-17 15:22:27 +0600

    Update listenMqtt.js

--- src/listenMqtt.js
+++ src/listenMqtt.js
@@ -220,7 +220,7 @@ function listenMqtt(defaultFuncs, api, ctx, globalCallback) {
 function parseDelta(defaultFuncs, api, ctx, globalCallback, v) {
   if (v.delta.class == "NewMessage") {
     //Not tested for pages
-    console.log(v.delta.attachments)
+    console.log(JSON.stringify(v.delta.attachments))
     if (ctx.globalOptions.pageID &&
       ctx.globalOptions.pageID != v.queue
     )
